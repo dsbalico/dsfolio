6a85602a519557be23cda4fb4404fd28b7996739
Daniel Shan Balico <danielshan.balico@gmail.com>
2024-06-22 23:55:13 +0800

    add hover message to image

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -5,11 +5,18 @@ function Hero() {
   return (
     <section id="hero" className="pb-28 pt-24 sm:pt-28 md:pt-44 flex px-6 lg:px-24">
       <div className="self-center">
-        <img
-          src={user_info.main.photo}
-          className="rounded-full w-20 mb-6 lg:hidden"
-          alt="Daniel Shan Balico Graduation Picture"
-        />
+        <div className="hs-tooltip [--placement:right] w-20 hs-tooltip-toggle">
+          <img
+            src={user_info.main.photo}
+            className="rounded-full mb-6 lg:hidden"
+            alt="Daniel Shan Balico Graduation Picture"
+          />
+
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity  inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm" role="tooltip">
+            Hello! 👋 How are you doing? 🤔
+          </span>
+        </div>
         <div className="flex gap-2 align-center flex-wrap md:flex-nowrap">
           <div className="lg:w-[80%] text-zinc-900 dark:text-zinc-100 self-center">
             <h2 className="text-xl">{user_info.main.role}</h2>
@@ -38,12 +45,17 @@ function Hero() {
             </div>
           </div>
 
-          <div className="hidden lg:block w-[480px] self-center">
+          <div className="hidden hs-tooltip [--placement:top-right] hs-tooltip-toggle lg:block w-[480px] self-center">
             <img
               className="rounded-[10%] transform rotate-3"
               src={user_info.main.photo}
               alt="Daniel Shan Balico Graduation Picture"
             />
+
+            {/* =========== TOOLTIP TEXT =========== */}
+            <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm" role="tooltip">
+              Hello! 👋 How are you doing? 🤔
+            </span>
           </div>
         </div>
       </div>
